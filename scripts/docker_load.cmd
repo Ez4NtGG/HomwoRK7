ddf59561b042df183c7d4fa32c7ec0cd6838208e
Ez4NtGG <145457084+Ez4NtGG@users.noreply.github.com>
2024-09-25 17:03:56 +0200

    Initial commit

---
+++ scripts/docker_load.cmd
@@ -0,0 +1,8 @@
+@echo off
+PUSHD ..\tests
+
+docker pull postgres:latest
+docker pull lexxai/web_hw_07:latest
+                  
+
+POPD
---
+++ scripts/docker_run.cmd
@@ -0,0 +1,6 @@
+@echo off
+
+docker_run_db.cmd
+docker_run_hw07.cmd
+                    
+
